be9302cd026409cee0da44a64ac29bd76e0e7f17
t-whelan <whelan.tionne@gmail.com>
2023-02-01 20:22:52 +0200

    Triangle drawing in C

---
+++ Triangle drawing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>>
+
+//C code that print out triangle using the printf function.
+
+int main ()
+{
+    printf("      / |\n");
+    printf("     /  |\n");
+    printf("    /   |\n");
+    printf("   /    |\n");
+    printf("  /     |\n");
+    printf(" /______|\n");
+
+    return 0;
+}
